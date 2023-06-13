9362346aad5a0755e00dcccf147ecfcdcc97c411
mir_faisal <mirfaisal08@gmail.comm>
2023-06-14 01:57:54 +0600

    add logout logic from firebase

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   // handel logout
   const handelLogout = () => {
     logoutFromAccount().then(() => {
-      showTost("Logout!");
+      showTost("Logout !");
     });
   };
   return (
